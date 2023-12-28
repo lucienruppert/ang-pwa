b9f07cd741119ea879e6d3b38ca7ee4bb3bfab26
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2023-12-28 17:40:18 +0100

    add isMobile function to render event logic correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { DataService } from '../data.service';
 
 @Component({
@@ -7,20 +7,46 @@ import { DataService } from '../data.service';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  joke: any;
+  public joke: any;
+  public pwaInstallStatus: Boolean | undefined = false;
+  public isMobile: boolean | undefined;
   constructor(private data: DataService) {}
 
   ngOnInit(): void {
+    this.loadJoke();
+    this.setPWAInstallStatus();
+    this.setIsMobile();
+  }
+
+  private setIsMobile(): void {
+    this.isMobile =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
+  }
+
+  private setPWAInstallStatus(): void {
+    this.pwaInstallStatus =
+      window.matchMedia('(display-mode: standalone)').matches ||
+      ((window.navigator as any).standalone as boolean);
+    if (this.pwaInstallStatus === undefined) this.pwaInstallStatus = false;
+  }
+
+  private loadJoke(): void {
+    try {
+      this.data
+        .getJokes()
+        .subscribe((result: any) => (this.joke = result['value']));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public handleTouchStart(): void {
     this.loadJoke();
   }
 
   public handleClick(): void {
     this.loadJoke();
-  }
-
-  private loadJoke(): void {
-    this.data
-      .getJokes()
-      .subscribe((result: any) => (this.joke = result['value']));
   }
 }
